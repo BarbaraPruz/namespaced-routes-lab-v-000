bcd92f177c977bee0ae7924270e411e31b86e084
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-08-01 17:57:22 +0000

    Done.

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,5 +1,11 @@
 class Preference < ActiveRecord::Base
   validates :artist_sort_order, inclusion: { in: [ "ASC", "DESC" ] }
   validates :song_sort_order, inclusion: { in: [ "ASC", "DESC" ] }
+
+  def allow_create_songs
+    Preference.find(1).allow_create_songs
+  end
+  def allow_create_songs=(value)
+    Preference.find(1).update(:allow_create_songs => value)
+  end
 end
-song_sort_order
