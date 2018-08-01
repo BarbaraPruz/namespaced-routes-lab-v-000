37bf1c36f1a6f4b62182ba086a3f947b1a7ad59e
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-08-01 20:57:48 +0000

    automatically backed up by learn

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -2,10 +2,13 @@ class Preference < ActiveRecord::Base
   validates :artist_sort_order, inclusion: { in: [ "ASC", "DESC" ] }
   validates :song_sort_order, inclusion: { in: [ "ASC", "DESC" ] }
 
+<<<<<<< HEAD
   def self.allow_create_songs
     Preference.find(1).allow_create_songs
   end
   def self.allow_create_artists
     Preference.find(1).allow_create_artists
   end
+=======
+>>>>>>> 0eacd1094e02ce396fcbaa5690b575752e22754b
 end
