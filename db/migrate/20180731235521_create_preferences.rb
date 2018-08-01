4df93bb024518fcf1571d3b0e7c34aa63c8f73f8
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-08-01 05:39:55 +0000

    Done.

--- db/migrate/20180731235521_create_preferences.rb
+++ db/migrate/20180731235521_create_preferences.rb
@@ -1,10 +1,10 @@
 class CreatePreferences < ActiveRecord::Migration
   def change
     create_table :preferences do |t|
-      t.string :song_sort_order
-      t.string :artist_sort_order
-      t.boolean :allow_create_songs
-      t.boolean :allow_create_artists
+      t.string :song_sort_order, default: 'ASC'
+      t.string :artist_sort_order, default: 'ASC'
+      t.boolean :allow_create_songs, default: true, null: false
+      t.boolean :allow_create_artists, default: true, null: false
       t.timestamps null: false
     end
   end
